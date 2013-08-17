5531f82035f1027c371e10ce7e054f06499232c2
bguiz <brendan.graetz@gmail.com>
2013-08-17 10:14:31 +1000

    =BG= create new model instance

---
+++ ember/emberjs.com-guide/js/todo/todo-ctrl.js
@@ -0,0 +1,18 @@
+Todos.TodosController = Ember.ArrayController.extend({
+	createTodo: function() {
+		//get title from input text field
+		var title = this.get('newTitle');
+		if (! title.trim()) {
+			return;
+		}
+
+		var todo = Todos.Todo.createRecord({
+			title: title,
+			isCompleted: false
+		});
+
+		this.set('newTitle', '');
+
+		todo.save(); //persist to datastore
+	}
+});
