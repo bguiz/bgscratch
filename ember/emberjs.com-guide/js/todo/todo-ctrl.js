a9c7a604c950b4d2a5793c7a7e45ae1edabfc667
bguiz <brendan.graetz@gmail.com>
2013-08-18 14:31:20 +1000

    =BG= delete a model

--- ember/emberjs.com-guide/js/todo/todo-ctrl.js
+++ ember/emberjs.com-guide/js/todo/todo-ctrl.js
@@ -50,5 +50,11 @@ Todos.TodoController = Ember.ObjectController.extend({
 	acceptChanges: function() {
 		this.set('isEditing', false);
 		this.get('model').save();
+	},
+
+	removeTodo: function() {
+		var todo = this.get('model');
+		todo.deleteRecord();
+		todo.save();
 	}
 });
