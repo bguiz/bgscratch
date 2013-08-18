669fc4126e73b084bd4786cf411989e5d388bab9
bguiz <brendan.graetz@gmail.com>
2013-08-18 15:12:39 +1000

    =BG= separate main page route to contain child route with todo list

--- ember/emberjs.com-guide/js/router.js
+++ ember/emberjs.com-guide/js/router.js
@@ -3,10 +3,18 @@
 //router
 
 Todos.Router.map(function() {
-	this.resource('todos', {path: '/'});
+	this.resource('todos', {path: '/'}, function() {
+		//child routes
+	});
 });
 
 Todos.TodosRoute = Ember.Route.extend({
+	model: function() {
+		return Todos.Todo.find();
+	}
+});
+
+Todos.TodosIndexRoute = Ember.Route.extend({
 	model: function() {
 		return Todos.Todo.find();
 	}
