b46a0a6cfb97458d8dbd3f62e1a9354617120ee3
bguiz <brendan.graetz@gmail.com>
2013-08-18 15:23:20 +1000

    =BG= custom route that uses a filtered model but same view as 'all' route

--- ember/emberjs.com-guide/js/router.js
+++ ember/emberjs.com-guide/js/router.js
@@ -4,7 +4,7 @@
 
 Todos.Router.map(function() {
 	this.resource('todos', {path: '/'}, function() {
-		//child routes
+		this.route('active');
 	});
 });
 
@@ -17,5 +17,22 @@ Todos.TodosRoute = Ember.Route.extend({
 Todos.TodosIndexRoute = Ember.Route.extend({
 	model: function() {
 		return Todos.Todo.find();
+	}
+});
+
+Todos.TodosActiveRoute = Ember.Route.extend({
+	model: function() {
+		return Todos.Todo.filter(function(todo) {
+			return (! todo.get('isCompleted'));
+		});
+	},
+
+	renderTemplate: function(controller) {
+		//override default render template, because convention over 
+		//config means that ember will look for a template called 
+		//'todos/active'
+		this.render('todos/index', {
+			controller: controller
+		});
 	}
 });
