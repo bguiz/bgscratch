b9d28d2efc0da899ff07b91649c4cc4b95d3206b
bguiz <brendan.graetz@gmail.com>
2013-09-15 20:40:11 +1000

    =BG= render index template through outlet of application template

--- ember/ordr/js/app.js
+++ ember/ordr/js/app.js
@@ -3,4 +3,5 @@ var App = Ember.Application.create();
 // namespace can be anything you like - but App is convention
 // this single line boots ember, set up all controllers, views, and template rendering that is needed
 // to see this, we'll actually need to render something - which we'll do in the HTML file
-
+// note that this single line of code also generated the index route and controller, and renders it templates
+// a single line of code does a lot
