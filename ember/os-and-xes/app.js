d8758b3e0c55c70df22189c849eed2e1bbbcc998
bguiz <brendan.graetz@gmail.com>
2013-09-15 12:37:36 +1000

    =BG= add templates and initial model state

--- ember/os-and-xes/app.js
+++ ember/os-and-xes/app.js
@@ -1,1 +1,29 @@
 App = Ember.Application.create({});
+
+App.IndexRoute = Ember.Route.extend({
+	setupController: function(controller, model) {
+		controller.send('reset');
+	}
+});
+
+App.IndexController = Ember.ArrayController.extend({
+	// properties
+
+	// actions
+	actions: {
+		reset: function() {
+			this.get('content').clear();
+			this.set('content', 
+				[{}, {}, {}, {}, {}, {}, {}, {}, {}].map(function() {
+				return App.Box.create();
+			}));
+			this.set('over', false);
+		}
+	}
+});
+
+App.Box = Ember.Object.extend({
+	userSelected: false,
+	computerSelected: false,
+	selected: Ember.computed.or('userSelected', 'computerSelected')
+});
