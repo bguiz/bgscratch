cf5d92d6d004e14c3dc4d5085a7afdd5b6f57d4e
bguiz <brendan.graetz@gmail.com>
2013-09-22 08:56:17 +1000

    =BG= authentication methods using bcrypt in auth controller

--- nodejs/sails/passport-demo/api/controllers/AuthController.js
+++ nodejs/sails/passport-demo/api/controllers/AuthController.js
@@ -5,13 +5,36 @@
  * @description	:: Contains logic for handling requests.
  */
 
+var passport = require('passport');
+
 module.exports = {
-
-  /* e.g.
-  sayHello: function (req, res) {
-    res.send('hello world!');
-  }
-  */
-  
-
+	//renders the login page
+	login: function(req, res) {
+		res.view();
+	},
+	//called when login form is submitted
+	process: function(req, res) {
+		var passportCb = passport.authenticate('local', function(err, user, info) {
+			if (err || !user) {
+				return res.send({
+					message: 'login failed'
+				});
+				res.send(err);
+			}
+			req.logIn(user, function(err) {
+				if (err) {
+					res.send(err);
+				}
+				return res.send({
+					message: 'login successful'
+				});
+			});
+		});
+		passportCb(req, res);
+	},
+	//called when logout is called
+	logout: function(req, res) {
+		req.logout();
+		res.send('logout successful');
+	}
 };
