82d23da71a6bce52a7319a9d8e59c1b925961072
bguiz <brendan.graetz@gmail.com>
2013-09-24 00:06:17 +1000

    =BG= authentication now invokes passport authentication function

--- nodejs/sails/passport-demo/api/policies/authenticated.js
+++ nodejs/sails/passport-demo/api/policies/authenticated.js
@@ -2,14 +2,11 @@
  * Allow any authenticated user.
  */
 module.exports = function (req, res, ok) {
-
-  // User is allowed, proceed to controller
-  if (req.session.authenticated) {
-    return ok();
-  }
-
-  // User is not allowed
-  else {
-    return res.send("You are not permitted to perform this action.", 403);
-  }
+	var isAunthenticated = req.isAunthenticated();
+	if (isAunthenticated) {
+		return next();
+	}
+	else {
+		return res.redirect('/login');
+	}
 };
